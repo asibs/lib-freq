ff96f2cab8a4ba662e95d7c55d3292c2d3509176
libfreq <freqlib@gmail.com>
2017-03-24 01:03:43 +0000

    Starting to bootstrap-ify app and front page

--- lib/lib_freq_utils.rb
+++ lib/lib_freq_utils.rb
@@ -28,4 +28,14 @@ module LibFreqUtils
     end
   end
 
+  def self.significant_round(n)
+    # Fun maths to round up to the next significant interval
+    # Eg. 1 -> 10, 15 -> 20, 99 -> 100, 101 -> 200, etc
+    # 1 - Take the log10 of n, floor it, giving how many times n is divisible by 10 (ensure it is always >= 1)
+    # 2 - Add 1 to n (ensure we always go above n), and round it up using negative the no. of tens as the no. of significant figures
+
+    tens = [Math.log10(n).floor, 1].max
+    (n+1).ceil(-tens)
+  end
+
 end
